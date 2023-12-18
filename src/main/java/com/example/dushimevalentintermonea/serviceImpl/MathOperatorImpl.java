fdad432701efbe9fa077b44c78749098b499a51f
valentindush <codesvalentin@gmail.com>
2023-12-18 09:07:38 +0200

    feat: added MathOperatorImpl

---
+++ src/main/java/com/example/dushimevalentintermonea/serviceImpl/MathOperatorImpl.java
@@ -0,0 +1,34 @@
+package com.example.dushimevalentintermonea.serviceImpl;
+
+import com.example.dushimevalentintermonea.exceptions.InvalidOperationException;
+import com.example.dushimevalentintermonea.services.IMathOperator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MathOperatorImpl implements IMathOperator {
+    @Override
+    public double doMath(double operand1, double operand2, String operation) throws InvalidOperationException {
+        if ("/".equals(operation) && operand2 == (double) 0) {
+            throw new InvalidOperationException("Cannot divide by 0");
+        }
+
+        switch (operation) {
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "**":
+                return Math.pow(operand1, operand2);
+            case "log":
+                return operand1 * Math.log10(operand2);
+            case "ln":
+                return operand1 * Math.log(operand2);
+            default:
+                throw new RuntimeException("Unknown Operation");
+        }
+    }
+}
