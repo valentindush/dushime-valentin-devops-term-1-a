991fb60fbccd84e5a6acc6e1a6069daa95622306
valentindush <codesvalentin@gmail.com>
2023-12-18 10:30:11 +0200

    feat: added E2E test using RestAssured to simulate browser interactions

--- src/test/java/com/example/dushimevalentintermonea/DushimevalentinTermoneAApplicationTests.java
+++ src/test/java/com/example/dushimevalentintermonea/DushimevalentinTermoneAApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.dushimevalentintermonea;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
-class DushimevalentinTermoneAApplicationTests {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DushimevalentinTermoneAApplicationTests {
+	@LocalServerPort
+	private int port;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testDoMathOperation_E2E() {
+		// Set the base URI for RestAssured
+		RestAssured.baseURI = "http://localhost:" + port;
 
+		// Given
+		double operand1 = 2;
+		double operand2 = 5;
+		String operation = "+";
+
+		// When
+		given()
+				.contentType(ContentType.JSON)
+				.body("{\"operand1\": " + operand1 + ", \"operand2\": " + operand2 + ", \"operation\": \"" + operation + "\"}")
+				.when()
+				.post("/api/v1/do_math")
+				.then()
+				.statusCode(200)
+				.body("calcResponse", equalTo(7.0f));
+	}
 }
