05ac0065ea0098d1cb998beabe5084d2c1645916
valentindush <codesvalentin@gmail.com>
2023-12-18 08:55:24 +0200

    chore: initial commit

---
+++ src/main/java/com/example/dushimevalentintermonea/DushimevalentinTermoneAApplication.java
@@ -0,0 +1,13 @@
+package com.example.dushimevalentintermonea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DushimevalentinTermoneAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DushimevalentinTermoneAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dushimevalentintermonea/DushimevalentinTermoneAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dushimevalentintermonea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DushimevalentinTermoneAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
